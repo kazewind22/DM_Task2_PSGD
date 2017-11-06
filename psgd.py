1fe86d4e6d10c793c69d07f6aa62c701efc44f73
Mengyuan Yang <b01902037@ntu.edu.tw>
2017-11-06 01:00:22 +0100

    full poly2 expansion

--- psgd.py
+++ psgd.py
@@ -1,9 +1,16 @@
 import numpy as np
 import random
 
+def self_outer(x):
+    return np.ravel(np.outer(x,x)) # flatten outer product matrix
+
 def transform(X):
     # Make sure this function works for both 1D and 2D NumPy arrays.
-    return X
+    # poly 2 transform
+    if X.ndim == 1:
+        return np.ravel(np.outer(X, X))
+    elif X.ndim == 2:
+        return np.apply_along_axis(self_outer, 1, X)
 
 def read_from_string(line):
     line = line.rstrip().split()
@@ -16,19 +23,19 @@ def mapper(key, value):
     # value: one line of input file
     random.seed(22)
     random.shuffle(value)
-    w = np.zeros(400) #init w as zero vector
+    w = np.zeros(160000) #init w as zero vector
     t = 1 #iteration
     C = 1 #parameter C
     num_ins = len(value) # number of instances
     for line in value:
         y, x = read_from_string(line)
+        x = transform(x)
         eta = 1.0 / np.sqrt(t)
         loss = 1 - y*np.dot(w,x)
         if loss > 0:
             w = w - eta*(w/num_ins - C*loss*y*x)
         else:
             w = w - eta*(w/num_ins)
-
     yield 0, w  # This is how you yield a key, value pair
 
 
@@ -36,8 +43,6 @@ def reducer(key, values):
     # key: key from mapper used to aggregate
     # values: list of all value for that key
     # Note that we do *not* output a (key, value) pair here.
-    W = np.array(values)
-    w = np.mean(W, axis=0)
-    print(len(w))
-    print(w.dtype)
+    w = np.array(values)
+    w = np.mean(w, axis=0)
     yield w
