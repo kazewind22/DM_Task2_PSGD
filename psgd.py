9ad3c5a1c2699284abb340d5e8939dc8e6fbf8c1
Mengyuan Yang <b01902037@ntu.edu.tw>
2017-11-09 19:22:20 +0100

    Random Fourier Feature

--- psgd.py
+++ psgd.py
@@ -1,33 +1,34 @@
 import numpy as np
-import random
 
-def self_outer(x):
-    return np.outer(x,x).ravel() # flatten outer product matrix
+DIM = 10000
+
+np.random.seed(22)
+W = np.sqrt(40.)*np.random.normal(0, 1, (DIM, 400))
+B = np.random.uniform(0, 2*np.pi, DIM)
 
 def transform(X):
     # Make sure this function works for both 1D and 2D NumPy arrays.
-    # poly 2 transform
+    # Random Fourier Feature
     if X.ndim == 1:
-        return np.outer(X, X).ravel()
+        return np.sqrt(2./DIM)*np.cos(W.dot(X)+B)
     elif X.ndim == 2:
-        return np.apply_along_axis(self_outer, 1, X)
+        return np.sqrt(2./DIM)*np.cos(X.dot(W.T)+B)
 
 def read_from_string(line):
-    line = line.rstrip().split()
-    y = float(line[0])
-    x = np.array([float(i) for i in line[1:]])
-    return y, x
+    nums = np.fromstring(line, dtype=float, sep=' ')
+    return nums[0], nums[1:]
 
 def mapper(key, value):
     # key: None
     # value: one line of input file
-    random.seed(22)
-    random.shuffle(value)
-    w = np.zeros(160000) #init w as zero vector
-    t = 1 #iteration
-    C = 1 #parameter C
+    np.random.seed(22)
+    np.random.shuffle(value)
+    w = np.zeros(DIM) #init w as zero vector
+    t = 0 #iteration
+    C = 10 #parameter C
     num_ins = len(value) # number of instances
     for line in value:
+        t += 1;
         y, x = read_from_string(line)
         x = transform(x)
         eta = 1.0 / np.sqrt(t)
